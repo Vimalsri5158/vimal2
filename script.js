509fdae413906495e6cc7de988795f932cd039db
Vimalsri5158 <125827626+Vimalsri5158@users.noreply.github.com>
2023-05-10 18:52:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,228 @@
+
+// alert("I'm JavaScript!");
+
+// alert("I'm Invoked");
+
+// alert("I'm JavaScript!");
+// alert('Hello');
+// alert(`World`);
+// alert(3+1+2);
+
+
+// let admin 
+// fname = "Guvi";
+// lname = "geek"
+// admin = fname+lname;
+// alert( admin );
+
+
+// let fname = "Guvi";
+// let lname = "geek";
+// let name = fname + " " + lname;
+// alert(`hello ${name}`);
+
+
+// let a = prompt("First number?");
+// let b = prompt("Second number?");
+// alert(a + b);
+
+// var a = "2" > "12";
+// a = parseInt(a); 
+// if (a) {
+//     console.log("Diffused")
+//   }
+//   else
+//   {
+//     console.log("Code is Blasted") 
+//   }
+
+
+// let a = prompt("Enter a number?");
+// a=parseInt(a);
+// if (a) {
+//  console.log( 'OMG it works for any number inc 0' );
+// }
+// else
+// {
+//  console.log( "Success" );
+// }
+
+// let value = prompt('How many runs you scored in this ball');
+// value = parseInt(value); 
+// if (value === 4) {
+//       console.log("You hit a Four");
+// } else if (value === 6) {
+//       console.log("You hit a Six");
+// } else {
+//       console.log("I couldn't figure out");
+// }
+
+
+// let message;
+
+// if (null || 2 || undefined )
+// {
+//  let message = "welcome boss";
+// }
+// else
+// {
+//  let message = "Go away";
+// }
+//   console.log(message);
+
+
+// let message;
+// let lock = 2;
+// lock=parseInt(message);
+// if (null || lock || undefined )
+// {
+//   message = "Go away";
+// }
+// else
+// {
+//  message = "welcome";
+// }
+//   console.log(message);
+
+
+// let i = 3;
+// while (i>0) {
+//   console.log(i);
+//   i--;
+// }
+
+// for (let i = 0; i <4; i++) {
+// for (let j = 1; j <= 3; j++) {
+//       let number = i*2+j ;
+//       console.log(number);
+//     }
+//   }
+
+
+// for (let num = 2; num <= 20; num += 2) {
+//     console.log(num);
+    // }
+
+
+    // let gifts = ["teddy bear", "drone", "doll"];
+    // for (let i = 0; i < gifts.length; i++) {
+    // console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    // }
+
+
+//     let countdown = 100;
+// while (countdown > 0) {
+// countdown--;
+// }
+// console.log("Bomb disarmed!");
+
+// <----------------------------------------->
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// for (var i = 0; i <Array.length; i++) {
+// console.log(numsArr.join(""))
+// }
+
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// numsArr.reverse();
+// console.log(numsArr.join(""));
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// numsArr.reverse();
+// console.log(numsArr.join(" "));
+
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// for (var i = 0; i <=numsArr.length; i++) {
+//  if(numsArr[i] %2 == 0 )
+//  {
+//  numsArr[i] = "even";
+//  }
+// }
+// console.log(numsArr);
+
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+// for (var i = 0; i <=numsArr.length; i++) {
+//     if(i%2===0){
+//         numsArr[i]="even";
+//     }
+// }
+// console.log(numsArr);
+
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// var sum=0;
+// for (var i = 0; i <numsArr.length; i++) {
+
+//  sum += numsArr[i]
+// }
+// console.log(sum);
+
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// var sum=0;
+// for (var i = 0; i <numsArr.length; i++) {
+//  if(numsArr[i]%2===0){
+//  sum += numsArr[i];
+//  }
+// }
+// console.log(sum);
+
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// var sum=100;
+// for (var i = 0; i <numsArr.length; i++) {
+//  if(numsArr[i]%2===0)
+//  {
+//  sum += numsArr[i];
+//  }
+//  else{
+//     sum -=numsArr[i];
+//  }
+//  }
+// console.log(sum);
+
+
+// var numsArr = [[1, 2, 3, 4, 5],[6, 7, 8, 9, 10, 11]];
+// var str_all = "";
+// for (var i = 0; i < numsArr.length; i++) {
+//   var inner_array = numsArr[i];
+//   for(var j = 0; j < inner_array.length; j++) {
+//     str_all += inner_array[j];
+//   }
+// }
+// console.log(str_all);
+
+// var numsArr = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11]];
+// let sum_odd=0;
+// let sum_even=0;
+
+// for(let i=0; i<numsArr.length; i++){
+//     let inner_array=numsArr[i];
+//     for(let j=0; j<inner_array.length; j++){
+//         if(inner_array[j]%2==0){
+//             sum_even +=inner_array[j];
+//         }else{
+//             sum_odd +=inner_array[j]
+//         }
+//     }
+// }
+// console.log(sum_even);
+// console.log(sum_odd);
+
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
